df383b3e5f85edd36bc37265536d277c09099688
infamousesco <84187715+infamousesco@users.noreply.github.com>
2022-03-30 19:23:50 -0400

    Updated validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,55 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const registerValidation = require('../validation');
- const bcrypt = require('bcrypt');
+const User = require("../models/User");
+const { registerValidation, loginValidation } = require("../validation");
+const bcrypt = require("bcrypt");
 
- const Joi = require('joi');
+const Joi = require("joi");
 
+router.post("/register", async (req, res) => {
+  //User validation
+  const userValidation = await registerValidation(req);
 
-router.post('/register', async (req, res) => {
+  //Check if user name exists
+  const userNameExist = await User.findOne({ userName: req.body.userName });
+  if (userNameExist)
+    return res.status(400).json({ message: "User Name already exists" });
 
-    const schema = Joi.object({
-        userName: Joi.string().min(6).max(10).required(),
-        email: Joi.string().min(6).required().email(),
-        password: Joi.string().min(6).required()
-    });
+  //Check for duplicate emails
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist)
+    return res.status(400).json({ message: "User already exists" });
 
-    //Hash passwords
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  //Hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    const user = new User({
-            userName: req.body.userName,
-            email: req.body.email,
-            password: hashedPassword
-        })
-    const userValidation = schema.validate(req.body)
-    
-    try{
-        if (!userValidation.error) {
-            const savedUser = await user.save()
-            res.status(200).json({message: "User created successfully."})
-        }else{
-            res.status(400).send(userValidation.error.details[0].message)
-        }
-        }catch (err){
-        res.status(400).json({message: "There was an error creating user."})
-       
+  const user = new User({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: hashedPassword,
+  });
+
+  try {
+    if (!userValidation.error) {
+      const savedUser = await user.save();
+      res.status(200).json({ message: "User created successfully." });
+    } else {
+      res.status(400).send(userValidation.error.details[0].message);
     }
-    //res.send(validation)
-    // const user = new User({
-    //         name: req.body.name,
-    //         email: req.body.email,
-    //         password: req.body.password
-    //     })
-    // try {
-    //     const savedUser = await user.save()
-    //     
-    // }catch (err) {
-    //    
-    // }
-     
-})
+  } catch (err) {
+    res.status(400).json({ message: "There was an error creating user." });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const loginValid = await loginValidation(req);
+  //Check for duplicate emails
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist)
+    return res.status(400).json({ message: "User already exists" });
+});
 
 module.exports = router;
--- validation.js
+++ validation.js
@@ -1,15 +1,27 @@
-
-const Joi = require('joi');
-
+const Joi = require("joi");
 
 const registerValidation = async (data) => {
-    const schema = Joi.object({
-        userName: Joi.string().min(6).max(10).required(),
-        email: Joi.string().min(6).required().email(),
-        password: Joi.string().min(6).required()
-    });
-    const userValidation = schema.validate(user);
-    return userValidation
-}
+  const schema = Joi.object({
+    userName: Joi.string().min(6).max(10).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
 
-module.exports = registerValidation;
+  const userValidation = schema.validate(data.body);
+
+  return userValidation;
+};
+
+const loginValidation = async (data) => {
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+
+  const loginValidation = schema.validate(data.body);
+
+  return loginValidation;
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
